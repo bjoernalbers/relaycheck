2d61941f7d8f2c4d76f856816681f6ebed2a400c
Björn Albers <mail@bjoernalbers.de>
2025-05-21 12:46:28 +0200

    Add http handler to pretty-print http requests.

--- main.go
+++ main.go
@@ -2,7 +2,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 )
 
 func init() {
@@ -10,5 +12,24 @@ func init() {
 }
 
 func main() {
-	log.Fatal("no implemented yet")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("----- New HTTP Request -----")
+
+		// Request-Line
+		fmt.Printf("%s %s %s\n", r.Method, r.URL.RequestURI(), r.Proto)
+
+		// Headers
+		fmt.Println("Headers:")
+		for name, values := range r.Header {
+			for _, value := range values {
+				fmt.Printf("  %s: %s\n", name, value)
+			}
+		}
+
+		// RemoteAddr
+		fmt.Printf("RemoteAddr: %s\n", r.RemoteAddr)
+
+		fmt.Println("----------------------------")
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
